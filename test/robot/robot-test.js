264608dde2e1eeb549086b7daf64371123725e40
Alexis Hope <alexis@cvofhope.com>
2018-01-11 09:43:02 +1100

    update robot test to jasmine

--- test/robot/robot-test.js
+++ test/robot/robot-test.js
@@ -1,11 +1,7 @@
 'use strict';
-
-const describe   = require('mocha').describe;
-const it         = require('mocha').it;
-const expect     = require('chai').expect;
-
 describe('Robot', () => {
-    it('should be placeable', () => {
-        expect(true).to.be(false);
+    it('should be of type Robot', () => {
+        const r = Robot.new();
+        expect(r).toEqual(jasmine.any(Robot));
     });
 });
