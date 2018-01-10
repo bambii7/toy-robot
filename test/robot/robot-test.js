f32ffba8b6dcdc161ada9890c4b1078e6700416a
Alexis Hope <alexis@cvofhope.com>
2018-01-10 19:10:39 +1100

    install mocha with a failing test case

---
+++ test/robot/robot-test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const describe   = require('mocha').describe;
+const it         = require('mocha').it;
+const expect     = require('chai').expect;
+
+describe('Robot', () => {
+    it('should be placeable', () => {
+        expect(true).to.be(false);
+    });
+});
