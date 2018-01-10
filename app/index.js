ac41351265248b76c849334645ca1b7cef5fd1a5
Alexis Hope <alexis@cvofhope.com>
2018-01-11 10:47:13 +1100

    add main index.js

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { configureStore, history } from './store/configureStore';
+import Root from './containers/Root';
+
+const store = configureStore();
+
+render(
+    <AppContainer>
+        <Root store={store} history={history}  />
+    </AppContainer>,
+    document.getElementById('root')
+);
+
