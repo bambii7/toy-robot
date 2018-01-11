2db3f8830080ace12d57cfe60f5acfe94acf2d17
Alexis Hope <alexis@cvofhope.com>
2018-01-11 13:02:04 +1100

    add table class

---
+++ app/components/Table.js
@@ -0,0 +1,14 @@
+
+class Table {
+    constructor() {
+        this.area = [
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0
+        ];
+    }
+}
+
+export default Table;
