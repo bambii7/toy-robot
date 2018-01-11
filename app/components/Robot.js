6f78136ba426a095a9a17d5a11bd1143030a1776
Alexis Hope <alexis@cvofhope.com>
2018-01-12 08:07:12 +1100

    update Robot static constants

--- app/components/Robot.js
+++ app/components/Robot.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 class Robot extends React.Component {
+    static FACES = {
+        NORTH: 'NORTH',
+        EAST: 'EAST',
+        SOUTH: 'SOUTH',
+        WEST: 'WEST'
+    };
+
+    static ROTATION = [
+        Robot.FACES.NORTH,
+        Robot.FACES.EAST,
+        Robot.FACES.SOUTH,
+        Robot.FACES.WEST
+    ];
+
     constructor(props) {
         super(props);
         this.facing = Robot.FACES.NORTH;
@@ -26,14 +40,5 @@ class Robot extends React.Component {
         return (<span>Beep Boop</span>);
     }
 }
-
-Robot.FACES = {
-    NORTH: 'NORTH',
-    EAST: 'EAST',
-    SOUTH: 'SOUTH',
-    WEST: 'WEST'
-};
-
-Robot.ROTATION = [Robot.FACES.NORTH, Robot.FACES.EAST, Robot.FACES.SOUTH, Robot.FACES.WEST];
 
 export default Robot;
