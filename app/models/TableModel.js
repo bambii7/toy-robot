aa32ce327a9263fadc00f492cea19c94d8d2855f
Alexis Hope <alexis@cvofhope.com>
2018-01-12 08:58:32 +1100

    add factory method to generate blank table array

---
+++ app/models/TableModel.js
@@ -0,0 +1,16 @@
+
+class TableModel {
+    static table = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ];
+
+    static tableFactory() {
+        return Array.from(TableModel.table);
+    }
+}
+
+export default TableModel;
