f182d1163120dcb84d5555aab2af5e4ef335ce2d
Alexis Hope <alexis@cvofhope.com>
2018-01-12 11:22:20 +1100

    add missing RobotModel

---
+++ app/models/RobotModel.js
@@ -0,0 +1,18 @@
+
+class RobotModel {
+    static FACES = {
+        NORTH: 'NORTH',
+        EAST: 'EAST',
+        SOUTH: 'SOUTH',
+        WEST: 'WEST'
+    };
+
+    static ROTATION = [
+        RobotModel.FACES.NORTH,
+        RobotModel.FACES.EAST,
+        RobotModel.FACES.SOUTH,
+        RobotModel.FACES.WEST
+    ];
+}
+
+export default RobotModel;
